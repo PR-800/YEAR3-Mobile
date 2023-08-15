f1041aa48f31a8cbd48f7343eb97f9b3288abcd0
PR-800 <88496264+PR-800@users.noreply.github.com>
2023-08-15 16:34:17 +0700

    lab04

---
+++ lab04/screens/GameOverScreen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, StyleSheet, Text, Button } from "react-native";
+
+const GameOverScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Game is Over!</Text>
+      <Text>Number of rounds : {props.round}</Text>
+      <Text>Correct Number was : {props.answer}</Text>
+      {/* ...เพิ่มโค้ด สรุปผลลัพธ์การเล่นเกม และมีปุ่มให้เริ่มเกมใหม่ได้... */}
+      <Button
+        title="NEW GAME"
+        onPress={() => {
+          // ...เพิ่มโค้ด...
+          props.onRestart();
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default GameOverScreen;
